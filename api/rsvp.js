a82347052549b5039fde63cd12f6d0b7869e844e
Harshit Maurya <harsitmaurya555@gmail.com>
2026-02-02 14:07:31 +0530

    last final

--- api/rsvp.js
+++ api/rsvp.js
@@ -33,3 +33,7 @@ export default async function handler(req, res) {
     return res.status(500).json({ error: "Internal Server Error" });
   }
 }
+return res.status(200).json({
+  success: true,
+  message: "Thanks for confirming ❤️"
+});
